2894ac5a9b3c8ffe264704c53d3d8cb09ba08b3f
wrkean <barracaadrianewew@gmail.com>
2026-06-29 13:43:09 +0800

    feat: set up cli using clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+#![allow(unused)]
+
+use clap::Parser;
+use std::path::PathBuf;
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+}
+
+#[derive(Parser)]
+struct Args {
+    /// Path to the input file
+    #[arg(help("Path na nagtuturo sa input file"))]
+    input: PathBuf,
 }
